592a5304d81ee682197d375e25a925d474ef6982
kmgibson <kmgibson@gwu.edu>
2018-06-25 17:32:56 -0400

    add pathoscope instructions and removed unnecessary files.

--- download_pathoscope_output.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p pathoscope_output
-while read k; do
-    scp your_username@login.colonialone.edu:path/to/Analysis/${k}/pathoid-sam-report.tsv pathoscope_output/${k}_pathoid-sam-report.tsv
-done <samps.txt
